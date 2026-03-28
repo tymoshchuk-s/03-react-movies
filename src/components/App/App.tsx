677c8d5da579bf5d119a10515bd3026e74148a9e
Paris <serhii.tymoshchuk@icloud.com>
2026-03-28 05:54:57 +0100

    complete ract movie project

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,7 +2,7 @@ import styles from './App.module.css'
 import SearchBar from '../SearchBar/SearchBar'
 import MovieGrid from '../MovieGrid/MovieGrid'
 import Loader from '../Loader/Loader'
-import ErrorMessage from '../Error/ErrorMessage'
+import ErrorMessage from '../ErrorMessage/ErrorMessage'
 import toast, { Toaster } from 'react-hot-toast';
 import fetchMovies from '../../services/movieService';
 import { useState } from 'react';
